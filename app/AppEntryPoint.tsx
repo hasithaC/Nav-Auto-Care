c6f3425cacf5e3a2185e61952a085a84f602e381
hasitha_c <hasitha_c@epiclanka.net>
2023-08-20 11:30:25 +0530

    refactor: app entry point

---
+++ app/AppEntryPoint.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Root from './Root';
+
+const AppEntryPoint = () => {
+  return <Root />;
+};
+
+export default AppEntryPoint;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import AppEntryPoint from './app/AppEntryPoint';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppEntryPoint);
