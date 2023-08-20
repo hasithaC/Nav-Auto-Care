a6e2484519bb024c147afa51e4f02f566a282b4e
hasitha_c <hasitha_c@epiclanka.net>
2023-08-20 13:28:21 +0530

    feat: stack navigator configuration

--- app/Root.tsx
+++ app/Root.tsx
@@ -1,12 +1,39 @@
-import {Text, View} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import NavigationStack from './navigation/NavigationStack';
+import {navigationRef} from './navigation/RootNavigation';
 import React from 'react';
+import {colors} from './theme';
 
-const Root = () => {
+const Root = (): React.JSX.Element => {
+  const navContainerTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: colors.BACKGROUND_COLOR,
+    },
+  };
   return (
-    <View>
-      <Text>Root</Text>
-    </View>
+    <>
+      <StatusBar
+        translucent={false}
+        backgroundColor={colors.BACKGROUND_COLOR}
+        barStyle={'default'}
+      />
+      <SafeAreaView style={styles.safeAreaView}>
+        <NavigationContainer ref={navigationRef} theme={navContainerTheme}>
+          <NavigationStack />
+        </NavigationContainer>
+      </SafeAreaView>
+    </>
   );
 };
 
 export default Root;
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: colors.BACKGROUND_COLOR,
+  },
+});
---
+++ app/features/Routing/screens/RoutingScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RoutingScreen = () => {
+  return (
+    <View>
+      <Text>RoutingScreen</Text>
+    </View>
+  )
+}
+
+export default RoutingScreen
+
+const styles = StyleSheet.create({})
